a293ebeb1f63ad02fef8b694c615407dd07fedb8
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-05 14:49:26 +0200

    Practice on UseReducer Hook

--- src/Components/ExerciseTwo Reducer and Context/ComponentF.jsx
+++ src/Components/ExerciseTwo Reducer and Context/ComponentF.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+
 const ComponentF = () => {
 	return (
 		<>
 			<p>Component F is with in Component E</p>
+
+			<h2>Component E Count -{counterContext.count} </h2>
+			<button onClick={() => counterContext.dispatch("increment")}>
+				Increment
+			</button>
+			<button onClick={() => counterContext.dispatch("decrement")}>
+				Decrement
+			</button>
+			<button onClick={() => counterContext.dispatch("reset")}>Reset</button>
 		</>
 	);
 };
