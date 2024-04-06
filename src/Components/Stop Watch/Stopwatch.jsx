b9a76c868e05e858555d24995f79e32d668a6181
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-06 19:14:55 +0200

    Add some exercises that i was doing for a pratice work on the useReducer Hook

--- src/Components/Stop Watch/Stopwatch.jsx
+++ src/Components/Stop Watch/Stopwatch.jsx
@@ -1,5 +1,25 @@
 import { useReducer, useRef, useEffect } from "react";
 
+const initialStateValue = {
+	isRunning: false,
+	time: 0,
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "start":
+			return { ...state, isRunning: true };
+		case "stop":
+			return { ...state, isRunning: false };
+		case "reset":
+			return { isRunning: false, time: 0 };
+		case "tick":
+			return { ...state, time: state.time + 1 };
+		default:
+			throw new Error();
+	}
+};
+
 const Stopwatch = () => {
 	return <div>Stopwatch</div>;
 };
